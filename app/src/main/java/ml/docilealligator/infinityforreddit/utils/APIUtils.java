3026922b41616e25b5fcc3568947c9fe026ac121
Luke Simmons <luke5083@live.com>
2024-08-19 21:38:43 -0500

    Update gradle.

--- app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
+++ app/src/main/java/ml/docilealligator/infinityforreddit/utils/APIUtils.java
@@ -30,7 +30,7 @@ public class APIUtils {
 
     public static final String CLIENT_ID_KEY = "client_id";
     public static final String CLIENT_SECRET_KEY = "client_secret";
-    public static final String CLIENT_ID = BuildConfig.REDDIT_API_KEY; // TODO: Check name
+    public static final String CLIENT_ID = BuildConfig.REDDIT_API_KEY;
     public static final String IMGUR_CLIENT_ID = "Client-ID cc671794e0ab397";
     public static final String REDGIFS_CLIENT_ID = "1828d0bcc93-15ac-bde6-0005-d2ecbe8daab3";
     public static final String REDGIFS_CLIENT_SECRET = "TJBlw7jRXW65NAGgFBtgZHu97WlzRXHYybK81sZ9dLM=";
